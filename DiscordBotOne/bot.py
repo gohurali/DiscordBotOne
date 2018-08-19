62a61d46bf2d15e56d35d43ba43b0237e188a943
gohurali <gohurali@live.com>
2018-08-19 02:38:37 -0700

    Checks for bad words and deletes and warns the user -- added header cmts

--- DiscordBotOne/bot.py
+++ DiscordBotOne/bot.py
@@ -1,3 +1,6 @@
+__author__ = "Gohur Ali"
+__version__ = "0.1"
+__status__ = "Initial Build Stage"
 import discord
 from discord.ext import commands
 import asyncio
@@ -12,6 +15,16 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+
+    # Words to represent bad words..
+    specificWords = ["soup", "food", "supreme", "backpack"]
+
+    # Loop looks for words within the array, if the "bad" word appears, delete and warn the user
+    for i in specificWords:
+        if(i in message.content):
+            await client.delete_message(message)
+            await client.send_message(message.channel, "No bad words please")
+
     if(message.content.startswith('!hello')):
         print('Bot said bye');
         await client.send_message(message.channel, "HI PAL")
@@ -22,4 +35,5 @@ async def on_message(message):
         user_id = message.author.id
         await client.send_message(message.channel,"Hi <@%s>" % (user_id))
 
+# Keep token empty with each push in order to keep bot secure!
 client.run("")
